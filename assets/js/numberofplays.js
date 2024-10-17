f2e76b6963a3efdb67b3c437fc138cdacb934a6f
martholamew <mspringett824@gmail.com>
2024-10-17 17:01:08 -0400

    added play count

---
+++ assets/js/numberofplays.js
@@ -0,0 +1,31 @@
+ // Function to perform the GET request
+ async function getNumberOfPlays(div,ratingKey) {
+    const url = 'http://localhost:8080/tautulli/playcount?ratingKey='+ratingKey; // Example API endpoint
+
+    try {
+        const response = await fetch(url); // Making the GET request
+        if (!response.ok) { // Check if the response is successful
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json(); // Parse the JSON data
+        console.log(data);
+        
+        document.getElementById(div).innerHTML = "Number of plays "+data.response.data[3].total_plays+secondsToHms(data.response.data[3].total_time);;
+
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+
+function secondsToHms(d) {
+    d = Number(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+
+    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+    return "</br>"+hDisplay + mDisplay + sDisplay ; 
+}
+
