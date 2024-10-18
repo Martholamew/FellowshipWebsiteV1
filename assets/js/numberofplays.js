7bdc0297a552139e04bfc0f4f45faefda856c11d
martholamew <mspringett824@gmail.com>
2024-10-17 20:43:29 -0400

    another test

--- assets/js/numberofplays.js
+++ assets/js/numberofplays.js
@@ -1,6 +1,6 @@
  // Function to perform the GET request
  async function getNumberOfPlays(div,ratingKey) {
-    const url = 'https://fellowshipbackend.onrender.com/tautulli/playcount?ratingKey='+ratingKey; // Example API endpoint
+    const url = 'https://fellowshipbackend.onrender.com/astautulli/playcount?ratingKey='+ratingKey; 
 
     try {
         const response = await fetch(url); // Making the GET request
